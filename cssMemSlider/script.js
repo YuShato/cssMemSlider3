a956769079821e2073ce4bfb3a7a85cab58cd2bc
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-10-17 02:53:05 +0300

    feat: add button onClick style changes (Sun, Oct 17, 2021 02:52:00 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,10 +1,12 @@
 const prevSlideElement = document.querySelector('.prev');
 const nextSlideElement = document.querySelector('.next');
 const slider = document.querySelector('.slider');
+const allBtnElements = document.querySelectorAll('.toggle-btn');
 const MAX_SLIDE_ARRAY_LENGTH = 4;
 let randomIndex = 0;
 let currentIndex;
-const allPhotos = ['./img/1.jpg', './img/2.jpg', './img/3.jpg', './img/4.jpg']
+let direction;
+
 
 const getPrevSlide = (currentIndex) => {
     randomIndex = currentIndex > 0 ? currentIndex -= 1 : MAX_SLIDE_ARRAY_LENGTH - 1;
@@ -17,27 +19,40 @@ const getNextSlide = (currentIndex) => {
 };
 
 const showPrevSlide = () => {
-    currentIndex = getPrevSlide(randomIndex);
-    slider.classList.add('slide-left');
-    setBg(currentIndex);
-    slider.addEventListener('animationend', () => {
-        slider.classList.remove('slide-left'); 
-    });
+    moveSlide('left')
 };
 
-const showNextSlide = () => {
+const moveSlide = (direction) => {
     currentIndex = getNextSlide(randomIndex);
-    slider.classList.add('slide-right');
-    setBg(randomIndex);
+    slider.classList.add(`slide-${direction}`);
+    setBg(currentIndex);
     slider.addEventListener('animationend', () => {
-        slider.classList.remove('slide-right'); 
+        slider.classList.remove(`slide-${direction}`);
     });
+}
+
+const showNextSlide = () => {
+    moveSlide('right')
 };
 
 const setBg = (randomIndex) => {
-    slider.style.backgroundImage = `url(${allPhotos[randomIndex]})`
+    slider.style.backgroundImage = `url(./img/${[randomIndex + 1]}.jpg)`
     return randomIndex;
 };
 
 prevSlideElement.addEventListener('click', showPrevSlide);
 nextSlideElement.addEventListener('click', showNextSlide);
+
+allBtnElements.forEach((btn) => {
+    btn.addEventListener('click', (evt) => {
+        prevIndex = randomIndex;
+        randomIndex = Number(evt.target.dataset.title);
+        let prevActiveBtnElement = document.querySelector('.btn-active');
+        prevActiveBtnElement.classList.toggle('btn-active');
+        allBtnElements[randomIndex].classList.toggle('btn-active');
+        direction = prevIndex > randomIndex ? 'left' : 'right';
+        setBg(randomIndex);
+        moveSlide(direction)
+        return randomIndex;
+    })
+})
